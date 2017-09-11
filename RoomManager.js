bce8419445b09e3365adcf7360c3da31e9d9b49d
Ruben Gil <gil.r419@gmail.com>
2017-09-10 20:16:57 -0400

    Create class to manage game rooms

--- RoomManager.js
+++ RoomManager.js
@@ -3,38 +3,44 @@ const uuidv4 = require('uuid/v4');
 class RoomManager {
   constructor(io) {
     this.io = io;
-    this.users = [];						//players
-  	this.socketIds = [];
-  	this.usersWaiting = [];		//players waiting for a game       MAY NOT BE NEEDED
-  	this.roomsNotFull = [];		//rooms that haven't been filled
-  	this.allFullRooms = [];		//filled rooms that are in use
+  	this.waitingQueue = [];		//players waiting for a game
   }
 
   joinRoom(socket) {
-    socket.join('waiting');
+    socket.join('waiting', () => {
+      let waitingQueue = this.waitingQueue;
+      waitingQueue.push(socket.id);
 
-    let rooms = this.io.sockets.adapter.rooms;
-    let waitingRoom = rooms['waiting']
-    let socketsInRoom = Object.keys(waitingRoom.sockets);
+      // Object with all rooms (key: room-name, value: room-object).
+      let rooms = this.io.sockets.adapter.rooms;
+      let waitingRoom = rooms['waiting']
 
-    console.log('Existing Rooms:', rooms)
-    console.log(rooms['waiting'], 'player(s) in waiting room.');
-    let target = this.io.sockets.connected[socketsInRoom[0]]
-    // console.log('Target Socket:', target);
-    console.log('Target Rooms:', target.rooms)
+      console.log(waitingRoom.length + ' player(s) in waiting room.');
 
-    if (socketsInRoom.length >= 2) {
-      let player1 = socketsInRoom[0].leave('waiting')
-      let player2 = socketsInRoom[1].leave('waiting')
-      let possRoomId = uuidv4();
+      // Object with all player sockets (key: socket-id, value: socket).
+      let allConnPlayers = this.io.sockets.connected
 
-      let roomId = possRoomId in rooms ? uuidv4() : possRoomId
 
-      player1.join(roomId);
-      player2.join(roomId);
+      if (waitingQueue.length >= 2) {
+        // Remove 2 players from waiting queue.
+        let player1Id = waitingQueue.shift();
+        let player2Id = waitingQueue.shift();
 
-      this.io.to(roomId).emit('startGame', { left: player1.id, right: player2.id })
-    }
+        // Remove selected players from waiting room.
+        let player1 = allConnPlayers[player1Id].leave('waiting');
+        let player2 = allConnPlayers[player2Id].leave('waiting');
+
+        let possRoomId = uuidv4();
+        let roomId = possRoomId in rooms ? uuidv4() : possRoomId
+
+        // Place selected players in a room where the match will be held.
+        player1.join(roomId);
+        player2.join(roomId);
+
+        this.io.to(roomId).emit('startGame', { left: player1Id, right: player2Id });
+      }
+    });
+
   }
 }
 
