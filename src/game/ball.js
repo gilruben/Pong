72d5b4986a9161034acd8eaff19834f8eebb4e47
Ruben Gil <gil.r419@gmail.com>
2017-09-10 21:56:38 -0400

    Clean up comments in ball.js

--- src/game/ball.js
+++ src/game/ball.js
@@ -12,13 +12,14 @@ function Ball(radius, centerX, centerY, speed, canvas, paddleDim) {
 	this.ballOutOfBounds = false;
 }
 
-//moves the ball
-//startPoint is an object with keys x and y, of where the ball will start
-//side will determine the direction, 1 is left, 2 is right;
-//paddle is an object that stores where the paddles are in the y-axis
-Ball.prototype.move = function (side, paddlePos, timePassed, allInfo){
+// Moves the ball.
+// side will determine the direction, 1 is left, 2 is right.
+// paddlePos is an object that stores where the paddles are in the y-axis.
+// timePassed is a number representing how many milliseconds have passed since
+// the last tick.
+Ball.prototype.move = function (side, paddlePos, timePassed){
 	this.timePassed += timePassed;
-	// console.log('Time Passed:', timePassed);
+
 	if(this.timePassed >= 1000/this.speed){
 		let moveX;
 		let moveY;
@@ -26,31 +27,21 @@ Ball.prototype.move = function (side, paddlePos, timePassed, allInfo){
 		let newCenterY;
 
 		if(!this.ballOutOfBounds){
-			//how much to move centerX and centerY
+			// How much to move centerX and centerY.
 			moveX = this.direction.x * Math.round(this.timePassed / (1000/this.speed));
 			moveY = this.direction.y * Math.round(this.timePassed / (1000/this.speed));
 
-			//new x and y's for the center if the previous center were to move moveX or moveY respectively
+			// New x and y's for the center if the previous center were to move moveX or moveY respectively.
 			newCenterX = moveX + this.centerX;
 			newCenterY = moveY + this.centerY;
 		}
 
 
-		// //redirects ball when it hits the edges of the x-axis by setting a new x coordinate
-		// if(newCenterX < this.radius){
-		// 	this.centerX = newCenterX * -1;
-		// 	this.direction.x *= -1;
-		// } else if(newCenterX > this.canvas.width - this.radius){
-		// 	this.centerX = (2 * this.canvas.width) - newCenterX;
-		// 	this.direction.x *= -1;
-		// } else {
-		// 	this.centerX = newCenterX
-		// }
-
 		if(newCenterX < (0 - (2 * this.radius)) || newCenterX > this.canvas.width + (2 * this.radius)){
 			let that = this;
 			this.ballOutOfBounds = true;
 
+			// Waiting 2 seconds before serving the ball.
 			setTimeout(function(){
 				that.centerX = that.startPoint.x;
 				that.centerY = that.startPoint.y;
@@ -66,7 +57,7 @@ Ball.prototype.move = function (side, paddlePos, timePassed, allInfo){
 			let rightPaddDist = canvasWidth - 20 - paddWidth
 
 			// Chooses LeftPaddDist or rightPaddDist depending on which direction the
-			// ball is currently headed
+			// ball is currently headed.
 			let contactPaddDist = (this.direction.x === -1) ? leftPaddDist : rightPaddDist;
 
 			// Where the ball will be after it has bounced off the paddle
@@ -79,7 +70,7 @@ Ball.prototype.move = function (side, paddlePos, timePassed, allInfo){
 		}
 
 
-		//redirects ball when it hits the edges of the y-axis by setting a new y coordinate
+		// Redirects ball when it hits the edges of the y-axis by setting a new y coordinate
 		if(!this.ballOutOfBounds){
 			if(newCenterY < this.radius){
 				this.centerY = newCenterY * -1
@@ -92,7 +83,7 @@ Ball.prototype.move = function (side, paddlePos, timePassed, allInfo){
 			}
 		}
 
-		//reset time passed
+		// Reset time passed
 		this.timePassed = 0;
 	}
 }
@@ -101,8 +92,7 @@ Ball.prototype.ballHitsPaddle = function (ballX, ballY, paddlePos) {
 	let isTouchingLeftPaddle = (ballX <= (20 + this.paddleDim.width)) && (ballX >= 20) && (ballY >= paddlePos.y1) && (ballY <= (paddlePos.y1 + this.paddleDim.height))
 	let isTouchingRightPaddle = (ballX >= (600 - 20 - this.paddleDim.width)) && (ballX <= 600 - 20) && (ballY >= paddlePos.y2) && (ballY <= (paddlePos.y2 + this.paddleDim.height))
 
-	if(isTouchingLeftPaddle || isTouchingRightPaddle){
-		console.log('Paddle Position:', paddlePos)
+	if (isTouchingLeftPaddle || isTouchingRightPaddle) {
 		return true;
 	} else {
 		return false;
