edab9a2f2e768b502cb881eff38f4bc00f610d6f
Ruben Gil <gil.r419@gmail.com>
2017-09-22 14:37:44 -0400

    Change ball class to ES6 class

--- src/game/ball.js
+++ src/game/ball.js
@@ -1,33 +1,35 @@
-function Ball(canvas) {
-	this.position = {};
-	this.radius;
-	this.ctx = canvas.getContext('2d');
-}
+class Ball {
+	constructor(canvas) {
+		this.position = {};
+		this.radius;
+		this.ctx = canvas.getContext('2d');
+	}
 
-Ball.prototype.setPosition = function (x, y) {
-	this.position = { x, y };
-}
+	setPosition(x, y) {
+		this.position = { x, y };
+	}
 
-Ball.prototype.setRadius = function (radius) {
-	this.radius = radius;
-}
+	setRadius(radius) {
+		this.radius = radius;
+	}
 
-Ball.prototype.render = function () {
-	const { position } = this;
+	render() {
+		const { position } = this;
 
-	if (position.x && position.y) {
-		this.ctx.beginPath();
-		this.ctx.fillStyle = "white";
-		this.ctx.arc(
-			this.position.x,
-			this.position.y,
-			this.radius,
-			0,
-			2 * Math.PI,
-			false
-		);
+		if (position.x && position.y) {
+			this.ctx.beginPath();
+			this.ctx.fillStyle = "white";
+			this.ctx.arc(
+				this.position.x,
+				this.position.y,
+				this.radius,
+				0,
+				2 * Math.PI,
+				false
+			);
 
-		this.ctx.fill();
+			this.ctx.fill();
+		}
 	}
 }
 
