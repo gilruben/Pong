ca5b2104020558af40727957c4c6524edb5a13e4
Ruben Gil <gil.r419@gmail.com>
2017-09-23 13:58:13 -0400

    Create Player class to store player data

---
+++ Player.js
@@ -0,0 +1,28 @@
+class Player {
+  constructor(socket) {
+    this.socket = socket;
+    this.gameRoomId = null;
+    this.playerNumber = 0;
+  }
+
+  setGameRoomId(gameRoomId) {
+    this.gameRoomId = gameRoomId;
+  }
+
+  setPlayerNumber(number) {
+    if (typeof number !== 'number') {
+      throw TypeError('Argument must be a number.');
+    } else if (number !== 0 || number !== 1 || number !== 2) {
+      throw Error('Argument must be 0, 1, or 2');
+    } else {
+      this.playerNumber = number;
+    }
+  }
+
+  getId() {
+    return this.socket.id;
+  }
+
+}
+
+module.exports = Player;
