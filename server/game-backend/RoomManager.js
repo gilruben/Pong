fd3a5cd4e6105eedde4755088e4b67ec092fff3f
Ruben Gil <gil.r419@gmail.com>
2017-10-07 17:09:49 -0400

    Incorporate Room class in RoomManager

--- server/game-backend/RoomManager.js
+++ server/game-backend/RoomManager.js
@@ -5,6 +5,7 @@ class RoomManager {
     this.io = io;
   	this.waitingQueue = [];		//players waiting for a game
     this.gameRooms = {};
+    this.Room = require('./Room')(io);
   }
 
   joinRoom(player) {
@@ -16,13 +17,15 @@ class RoomManager {
       waitingQueue.push(player);
 
       // Object with all rooms (key: room-name, value: room-object).
-      let rooms = this.io.sockets.adapter.rooms;
+      let rooms = io.sockets.adapter.rooms;
       let waitingRoom = rooms['waiting']
 
       console.log(waitingRoom.length + ' player(s) in waiting room.');
 
 
       if (waitingQueue.length >= 2) {
+        const { Room } = this;
+
         // Remove 2 players from waiting queue.
         const player1 = waitingQueue.shift();
         const player2 = waitingQueue.shift();
@@ -31,21 +34,14 @@ class RoomManager {
         player1.socket.leave('waiting');
         player2.socket.leave('waiting');
 
-
         let possRoomId = shortid.generate();
         let roomId = possRoomId in rooms ? shortid.generate() : possRoomId
 
-        // Place selected players in a room where the match will be held.
-        player1.socket.join(roomId);
-        player2.socket.join(roomId);
+        const room = new Room(roomId);
+        room.join(player1, player2);
+        
+        this.gameRooms[roomId] = room;
 
-        player1.setGameRoomId(roomId);
-        player2.setGameRoomId(roomId);
-
-        this.gameRooms[roomId] = {
-          player1: player1,
-          player2: player2
-        };
 
         io.to(roomId).emit('startGame', {
           left: player1.getId(),
@@ -55,8 +51,14 @@ class RoomManager {
     });
   }
 
+  leaveRoom(gameRoomId, playerId) {
+    const gameRoom = this.gameRooms[gameRoomId];
+
+    gameRoom.leave(playerId);
+  }
+
   getGameRoomPlayers(gameRoomId) {
-    return this.gameRooms[gameRoomId];
+    return this.gameRooms[gameRoomId].players;
   }
 }
 
